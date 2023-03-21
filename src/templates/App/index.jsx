9a0522b507b56430f1c2087b83e39fbb11dc219b
Nilton Moraes Neto <nilton.nmn@gmail.com>
2023-03-21 09:30:05 -0300

    SotryBook e Jest para testes

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,10 +1,11 @@
+import { Heading } from '../../components/Heading';
 import * as Styled from './styles';
 
 function Home() {
   return (
     <div className="App">
       <Styled.Wrapper>
-        <h1>Hello World</h1>
+        <Heading>TEXTO TESTE</Heading>
       </Styled.Wrapper>
     </div>
   );
